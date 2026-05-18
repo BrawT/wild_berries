0befe64b6cc56bf294c31e6106b0337c8412e83f
Rina Ostrovsky <a_rin.ostr@outlook.com>
2026-05-19 01:09:08 +0300

    Add files via upload
    update search.js with new logic
    feat: log input value on button click instead of every input change

--- js/search.js
+++ js/search.js
@@ -2,8 +2,10 @@ const search = function () {
   const input = document.querySelector(".search-block > input");
   const searchBtn = document.querySelector(".search-block > button");
 
-  input.addEventListener("input", (event) => {
-    console.log(event.target.value);
+  // Listen for a click event on the button
+  searchBtn.addEventListener("click", () => {
+    // Log the current input value to the console
+    console.log(input.value);
   });
 };
 
